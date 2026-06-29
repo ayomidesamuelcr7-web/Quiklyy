2a0e9f9b3683ac0f7e80b110f3c0f13f1b47b1a9
ayomidesamuelcr7-web <ayomidesamuelcr7@gmail.com>
2026-06-29 11:57:05 +0100

    Add global 3px margin to the application body

--- quiklyy-app/src/components/consumer/PurchaseScreen.jsx
+++ quiklyy-app/src/components/consumer/PurchaseScreen.jsx
@@ -3,7 +3,7 @@ import RecentOrders from './RecentOrders';
 
 export default function PurchaseScreen({ session }) {
   return (
-    <div className="animate-slide-up pb-32 pt-6 px-1">
+    <div className="animate-slide-up pb-32 pt-6 px-0">
       <RecentOrders session={session} />
     </div>
   );
